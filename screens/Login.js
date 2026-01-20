d1f8687e6311f95cdbce143ad516d90b06f2336f
Patricia Lyca Castro <patricialycacastro@gmail.com>
2026-01-20 19:47:54 +0800

    styles, login

---
+++ screens/Login.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "../stylesheets/styles";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
