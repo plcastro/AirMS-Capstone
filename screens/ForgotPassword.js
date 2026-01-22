40b65e3018bd8cec92b0a7463e15a6ffe2b717a7
Patricia Lyca Castro <patricialycacastro@gmail.com>
2026-01-22 19:12:43 +0800

    login style

---
+++ screens/ForgotPassword.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "../stylesheets/styles";
+
+export default function ForgotPassword() {
+  return (
+    <View>
+      <Text>ForgotPassword</Text>
+    </View>
+  );
+}
