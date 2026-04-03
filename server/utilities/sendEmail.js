7d9b31afc2b957c1ff4836794006e1e0517c78a6
Patricia <patricialycacastro@gmail.com>
2026-04-03 15:01:49 +0800

    fixed error

--- server/utilities/sendEmail.js
+++ server/utilities/sendEmail.js
@@ -2,26 +2,31 @@ const nodemailer = require("nodemailer");
 
 module.exports = async ({ to, subject, text, html }) => {
   if (!to) throw new Error("No recipient defined for email");
+
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: Number(process.env.EMAIL_PORT) === 465, // true for 465, false for 587
+      secure: Number(process.env.EMAIL_PORT) === 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
       connectionTimeout: 5000,
     });
+
     await transporter.verify();
-    await transporter.sendMail({
+
+    const info = await transporter.sendMail({
       from: `"AirMS Support" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
       html,
     });
-    console.log("Message sent: %s", info.messageId);
+
+    // console.log("Message sent: %s", info.messageId);
+
     return info;
   } catch (error) {
     console.error("EMAIL_ERROR_DETAIL:", error);
