40c4344967e3ce2afd48f3d07495f0968a9ac6a0
Patricia <patricialycacastro@gmail.com>
2026-04-03 13:21:18 +0800

    apibase

--- client-web/src/utils/API_BASE.js
+++ client-web/src/utils/API_BASE.js
@@ -1,5 +1,5 @@
 const localUrl = "http://localhost:8000";
-const productionUrl = "https://backend-six-chi-43.vercel.app/";
+const productionUrl = "https://backend-six-chi-43.vercel.app";
 
 export const API_BASE =
   process.env.NODE_ENV === "development" ? localUrl : productionUrl;
