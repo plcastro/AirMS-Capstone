57fa15576983cff401349961d26ef5226244e19c
Patricia Lyca Castro <patricialycacastro@gmail.com>
2026-05-12 17:45:56 +0800

    Merge branch 'testing' of https://github.com/plcastro/AirMS-Capstone into testing

--- server/config/jobTitles.js
+++ server/config/jobTitles.js
@@ -2,8 +2,9 @@ const permissions = require("./permissions");
 
 const jobTitles = {
   Mechanic: [
-    permissions.TASKS_READ,
-    permissions.TASKS_UPDATE,
+    permissions.USERS_READ,
+    permissions.TASKS_READ_OWN,
+    permissions.TASKS_UPDATE_OWN,
     permissions.MAINTENANCELOG_CREATE,
     permissions.MESSAGE_READ,
     permissions.MESSAGE_SEND,
@@ -15,6 +16,7 @@ const jobTitles = {
     permissions.MESSAGE_READ,
     permissions.MESSAGE_SEND,
   ],
+
   "Warehouse Department": [
     permissions.MESSAGE_READ,
     permissions.MESSAGE_SEND,
@@ -23,30 +25,49 @@ const jobTitles = {
     permissions.WAREHOUSE_UPDATE,
     permissions.WAREHOUSE_CANCEL,
   ],
+
   "Officer-In-Charge": [
-    permissions.MESSAGE_READ,
-    permissions.MESSAGE_SEND,
+    permissions.REPORTS_READ,
+    permissions.REPORTS_EXPORT,
+
     permissions.WAREHOUSE_READ,
     permissions.WAREHOUSE_CREATE,
     permissions.WAREHOUSE_UPDATE,
     permissions.WAREHOUSE_CANCEL,
+
+    permissions.MESSAGE_READ,
+    permissions.MESSAGE_SEND,
   ],
+
   "Maintenance Manager": [
-    permissions.TASKS_READ,
+    permissions.TASKS_READ_ALL,
     permissions.TASKS_CREATE,
-    permissions.TASKS_UPDATE,
+    permissions.TASKS_UPDATE_ALL,
+
     permissions.MAINTENANCELOG_UPDATE,
+
+    permissions.MECHANICS_READ,
+    permissions.MECHANICS_ASSIGN,
+
     permissions.MESSAGE_READ,
     permissions.MESSAGE_SEND,
   ],
 
   Admin: [
     permissions.ADMIN_PANEL,
+
+    permissions.USERS_READ,
     permissions.USERS_CREATE,
     permissions.USERS_UPDATE,
-    permissions.USERS_READ,
-    permissions.TASKS_READ,
-    permissions.TASKS_UPDATE,
+    permissions.USERS_DELETE,
+
+    permissions.TASKS_READ_ALL,
+    permissions.TASKS_CREATE,
+    permissions.TASKS_UPDATE_ALL,
+    permissions.TASKS_DELETE,
+
+    permissions.ACTIVITYLOGS_READ,
+
     permissions.MESSAGE_READ,
     permissions.MESSAGE_SEND,
   ],
