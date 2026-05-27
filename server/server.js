59dcdf3e8bdb3566960b579d6d413dad13b360f0
Patricia Lyca Castro <patricialycacastro@gmail.com>
2026-05-27 22:10:39 +0800

    fix: update CORS preflight handling to support all routes

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
 app.use(
   cors(corsOptions),
 );
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.get("/api/events/stream", subscribeSSE);
 
