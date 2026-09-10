748fcd894a36aa018939502fc741e349a094b86d
Patricia Lyca Castro <patricialycacastro@gmail.com>
2026-09-10 10:24:41 +0800

    fixes

--- client-web/src/main.jsx
+++ client-web/src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+window.addEventListener("vite:preloadError", (event) => {
+  event.preventDefault();
+
+  if (sessionStorage.getItem("airms-preload-reloaded") === "true") {
+    return;
+  }
+
+  sessionStorage.setItem("airms-preload-reloaded", "true");
+  window.location.reload();
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
